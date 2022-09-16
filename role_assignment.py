6866be5f8a44e0ce84340e8574074a831b154ce7
Ian Gingrich <gingrichian@gmail.com>
2022-09-16 01:24:35 -0700

    Fill out other commands Complex arguments not supported. Users only referenceable by specific ID. Role quantity not supported.

--- role_assignment.py
+++ role_assignment.py
@@ -22,8 +22,9 @@ Token = config['Token']
 Rooms = {}
 
 
-# Note that the requested Permissions number should be: 399163731024
+# Note that the requested Permissions number was created with: 399163731024
 requested_intents = discord.Intents.default()
+# Requires turning on Message Content Intent on the bot page. of discord.com/developers
 requested_intents.message_content = True
 bot=discord.ext.commands.Bot(command_prefix=PREFIX, intents=requested_intents)
 
@@ -33,42 +34,49 @@ async def create_room(ctx, *args):
   print(args)
   if not args:
     print("Need room name.")
-  Rooms[args[0]]=Room(moderator=ctx)
+  Rooms[args[0]]=Room(moderator=ctx.author.id)
 
 @bot.command(name="join")
-async def join_room(ctx, *args):
-  print("joining")
+async def join_room(ctx, args):
+  Rooms[args[0]].add_player(ctx.author.id)
 
 @bot.command(name="leave")
-async def leave_room(ctx, *args):
-  print("leaving")
+async def leave_room(ctx, args):
+  Rooms[args[0]].remove_player(ctx.author.id)
 
 @bot.command(name="remove")
-async def remove_player(ctx, *args):
-  print()
+async def remove_player(ctx, args):
+  Rooms[args[0]].remove_player(args[1])
 
 @bot.command(name="invite")
-async def invite_player(ctx, *args):
-  print()
+async def invite_player(ctx, args):
+  Rooms[args[0]].add_player(args[1])
 
 @bot.command(name="add")
-async def add_role(ctx, *args):
-  print()
+async def add_role(ctx, args):
+  Rooms[args[0]].add_role(args[1])
 
 @bot.command(name="subtract")
-async def subtract_role(ctx, *args):
-  print()
+async def subtract_role(ctx, args):
+  Rooms[args[0]].remove_role(args[1])
 
 @bot.command(name="start")
-async def assign_roles(ctx, *args):
-  print()
+async def assign_roles(ctx, args):
+  game=Rooms[args[0]].start_game()
+  members=ctx.guild.members()
+  members_map = {x.id : x for x in members}
+  members[Rooms[args[0]].moderator].send(game)
+  for player,role in game.items:
+    members[player].send("Game: %s, Role: %s" % (args[0], role))
 
 @bot.command(name="list")
-async def list_room_info(ctx, *args):
-  print()
+async def list_room_info(ctx, args):
+  ctx.send("Room %s Info:\n%s" % (args[0], str(Rooms[args[0]].get_roles())))
 
 @bot.command(name="delete")
-async def delete_room(ctx, *args):
-  print()
+async def delete_room(ctx, args):
+  if args[0] in Rooms:
+    del Rooms[args[0]]
+    ctx.send("Deleted Room %s" % args[0])
 
 bot.run(Token)
