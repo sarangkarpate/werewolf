3b6494187c32956cb352887be471d67106a71ec6
Sarang Karpate <karpatesj@gmail.com>
2022-09-16 21:04:12 -0700

    - Separated token/prefix fetching into a utils class - Setup Heroku framework ProcFile

---
+++ util/utils.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+TOKEN_KEY = 'Token'
+PREFIX_KEY = 'Prefix'
+
+
+def fetch_bot_token():
+    """
+    Fetches bot's token either from a Config file if present / otherwise assumes environment variable has token set
+    This is useful for Deploying to cloud services and test locally as well
+    To test locally replace <PlaceHolder> in config.json with Token
+    :return: Token (String)
+    """
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+        token = config[TOKEN_KEY]
+        if token == "<PlaceHolder>":
+            # This means that it's deployed in production and needs to fetch token from Env variable
+            print("Fetching bot token from enviroment variable:", TOKEN_KEY)
+            token = os.environ[TOKEN_KEY]
+            print(token, type(token))
+        return token
+
+
+def fetch_bot_command_prefix():
+    """
+    Fetches bot command prefix (to be used in Discord app)
+    :return: String
+    """
+    with open("config.json") as config_file:
+        config = json.load(config_file)
+        return config[PREFIX_KEY]
