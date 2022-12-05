762e6edd8d2b0e1688f1398d762dbd2e608c0a49
Ammar Hussein <ammar.atef45@gmail.com>
2022-12-04 20:51:01 -0800

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,4 +8,7 @@ fi
 
 mkdir -p $FOLDER
 
+# if this is a clean installation, the line below exits with code 1
+# exit 0 allows the script to be seen as successful by the caller during deployment
 pkill -f role_assignment.py
+exit 0
